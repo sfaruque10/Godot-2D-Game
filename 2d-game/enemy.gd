44ceec51bc858dd486f7878e83bd6f896437ebf2
sfaruque10 <98576322+sfaruque10@users.noreply.github.com>
2025-10-30 18:38:53 -0400

    Player Errors
    Fixed errors with player body.

--- 2d-game/enemy.gd
+++ 2d-game/enemy.gd
@@ -1,9 +1,11 @@
+extends CharacterBody2D
+
 # enemy
 
 
-func _on_wind_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+#func _on_wind_body_entered(body: Node2D) -> void:
+	#pass # Replace with function body.
 
 
-func _on_wind_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+#func _on_wind_body_exited(body: Node2D) -> void:
+	#pass # Replace with function body.
