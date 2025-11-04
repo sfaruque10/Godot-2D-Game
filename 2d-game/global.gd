2c63ac2db9bcfcc91a03e307b78458b2ccae4b46
sfaruque10 <98576322+sfaruque10@users.noreply.github.com>
2025-11-04 02:02:01 -0500

    Win Message
    Implemented win message

--- 2d-game/global.gd
+++ 2d-game/global.gd
@@ -4,6 +4,6 @@ var gravity_direction = 1 # normal gravity or flipped gravity
 var has_dash = false #Player does not have immediate access to dash
 var max_dashes = 1
 var max_jumps = 1
-
+var won = false
 #func change_text(message: String):
 	#$PlayerCommands/TextEdit.text = message
