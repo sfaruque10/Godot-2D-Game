b64fdb01cbc494b0a200f28adf4d687d48ea70aa
sfaruque10 <98576322+sfaruque10@users.noreply.github.com>
2025-11-11 18:48:09 -0500

    More Instructions
    Added more instructions.

--- 2d-game/player_commands.gd
+++ 2d-game/player_commands.gd
@@ -2,5 +2,5 @@ extends Control
 
 func _ready():
 	var tween = create_tween()
-	tween.tween_property($TextEdit, "modulate:a", 0.0, 5)
+	tween.tween_property($TextEdit, "modulate:a", 0.0, 15)
 	tween.finished.connect(func(): $TextEdit.visible = false)
