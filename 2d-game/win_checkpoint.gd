7120e670863c3927fe861358df8c43fbc570561b
JayMCook <jmcook5520@gmail.com>
2025-12-16 18:41:30 -0500

    Game end flag and sound added

--- 2d-game/win_checkpoint.gd
+++ 2d-game/win_checkpoint.gd
@@ -3,6 +3,8 @@ extends Area2D
 func _on_body_entered(_body: Node2D) -> void:
 	if Global.won == false:
 		Global.won = true
+		# game comletion audio queue
+		$WinSound.play()
 		# display label indicating the player won
 		var tween: Tween = create_tween()
 		tween.tween_property($WinLabel, "modulate", Color.WHITE, 1)
@@ -10,3 +12,4 @@ func _on_body_entered(_body: Node2D) -> void:
 		var tween1: Tween = create_tween()
 		tween1.tween_property($WinLabel, "modulate:a", 0.0, 1)
 		tween1.finished.connect(func(): $WinLabel.visible = false)
+		
