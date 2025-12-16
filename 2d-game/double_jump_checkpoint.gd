5bd7214bc92c5c20d1a42f61afd801c765db29ec
sfaruque10 <98576322+sfaruque10@users.noreply.github.com>
2025-12-15 20:19:56 -0500

    Double Jump
    Added double jump symbol.

--- 2d-game/double_jump_checkpoint.gd
+++ 2d-game/double_jump_checkpoint.gd
@@ -9,8 +9,13 @@ func _on_body_entered(_body: Node2D) -> void:
 		Global.max_jumps = 2
 		# Notification of ability to double jump
 		var tween: Tween = create_tween()
+		var tween2: Tween = create_tween()
 		tween.tween_property($DoubleJumpLabel, "modulate", Color.WHITE, 1)
+		tween2.tween_property($Sprite2D, "modulate", Color.WHITE, 1)
 		await get_tree().create_timer(5.0).timeout
 		var tween1: Tween = create_tween()
+		var tween3: Tween = create_tween()
 		tween1.tween_property($DoubleJumpLabel, "modulate:a", 0.0, 1)
+		tween3.tween_property($Sprite2D, "modulate:a", 0.0, 1)
 		tween1.finished.connect(func(): $DoubleJumpLabel.visible = false)
+		tween3.finished.connect(func(): $Sprite2D.visible = false)
