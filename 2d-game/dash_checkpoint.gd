6c2a8bf5a523916df6d9577f70aa282d7985db0f
sfaruque10 <98576322+sfaruque10@users.noreply.github.com>
2025-12-15 20:06:17 -0500

    Dash

--- 2d-game/dash_checkpoint.gd
+++ 2d-game/dash_checkpoint.gd
@@ -5,8 +5,13 @@ func _on_body_entered(_body: Node2D) -> void:
 		Global.has_dash = true
 		# Notification of ability to dash
 		var tween = create_tween()
+		var tween2 = create_tween()
 		tween.tween_property($DashLabel, "modulate", Color.WHITE, 1)
+		tween2.tween_property($Sprite2D, "modulate", Color.WHITE, 1)
 		await get_tree().create_timer(5.0).timeout
 		var tween1 = create_tween()
+		var tween3 = create_tween()
 		tween1.tween_property($DashLabel, "modulate:a", 0.0, 1)
 		tween1.finished.connect(func(): $DashLabel.visible = false)
+		tween3.tween_property($Sprite2D, "modulate:a", 0.0, 1)
+		tween3.finished.connect(func(): $Sprite2D.visible = false)
